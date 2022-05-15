b89cd619204831f2c75bbe363da6750bbccb8bef
Jamaluddin Rumi <gangsar.swapurba@gmail.com>
2022-05-15 18:49:27 +0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { MotionPlugin } from "@vueuse/motion";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(MotionPlugin);
+
+app.mount("#app");
