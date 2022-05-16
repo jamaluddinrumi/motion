1d7d4200c43347b353d860b844c15f04193200b9
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-05-16 22:27:40 +0700

    replace link text in home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <ul style="width: fit-content; text-align: left;list-style: none;margin-bottom: 5rem;">
-    <li><router-link to="/benang">benang</router-link></li>
-    <li><router-link to="/speedometer">speedometer</router-link></li>
-    <li><router-link to="/kipas-mask">kipas-mask</router-link></li>
+    <li><router-link to="/benang">Benang</router-link></li>
+    <li><router-link to="/speedometer">Speedometer</router-link></li>
+    <li><router-link to="/kipas-mask">Kipas & Masker</router-link></li>
   </ul>
   <router-view></router-view>
 </template>
