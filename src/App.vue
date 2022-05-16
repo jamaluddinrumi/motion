883c2125d36eb98885a0a566d9e71091db9981cb
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-05-16 20:55:23 +0700

    place content center

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul style="width: fit-content; text-align: left;list-style: none;">
+  <ul style="width: fit-content; text-align: left;list-style: none;margin-bottom: 5rem;">
     <li><router-link to="/benang">benang</router-link></li>
     <li><router-link to="/speedometer">speedometer</router-link></li>
     <li><router-link to="/kipas-mask">kipas-mask</router-link></li>
