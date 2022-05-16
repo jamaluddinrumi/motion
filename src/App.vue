d2114b6c4ea55bce25e04aceb4e8a0de2099f769
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-05-16 22:41:32 +0700

    add a link to home in home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <ul style="width: fit-content; text-align: left;list-style: none;margin-bottom: 5rem;">
-    <li><router-link to="/benang">Benang</router-link></li>
-    <li><router-link to="/speedometer">Speedometer</router-link></li>
-    <li><router-link to="/kipas-mask">Kipas & Masker</router-link></li>
+  <ul
+    style="width: fit-content; text-align: left;list-style: none;margin-bottom: 5rem;"
+  >
+    <li>
+      <router-link to="/"><b>Home</b></router-link>
+    </li>
+    <li>
+      <router-link to="/benang"><b>Benang</b></router-link>
+    </li>
+    <li>
+      <router-link to="/speedometer"><b>Speedometer</b></router-link>
+    </li>
+    <li>
+      <router-link to="/kipas-mask"><b>Kipas & Masker</b></router-link>
+    </li>
   </ul>
   <router-view></router-view>
 </template>
